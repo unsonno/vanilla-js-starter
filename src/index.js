3e797c0bab0f8f140168d4680d455acfec4db5e5
unsonno <168208419+unsonno@users.noreply.github.com>
2024-06-17 00:19:23 -0600

    adds

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-// Inserte el código aquí
+import {getData,postData,deleteData,putData} from "./metodos";
+
+
+let contador = document.getElementById("contador");
+let btnadd = document.getElementById("btnadd");
+let display = document.getElementById("display");
+
+let newTask = {
+    description: inputadd.value,
+    state: false,
+    creation: new Date (Date.now()).toLocaleString()
+}
+
+let addTask = async (e) => { 
+    e.preventDefault();
+    if (inputadd.value.trim("") != "") {
+        let newTask
+     }
+      inputadd.value = "";
+      let tasks = await postData(newTask);
+      showtasks(tasks);
+    }
 
